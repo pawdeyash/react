a90de3727b366eaf97576cfb5a6472d831a3d321
pawdeyash <yashpawde143@gmail.com>
2025-07-30 16:52:49 +0530

    Upload

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react'
+
+function App() {
+  const [Text , setText] = useState("");
+
+  function changeHolder(event) {
+    // console.log(event.target);
+    setText(event.target.value)
+  }
+  return (
+    <div className="App">
+      <form>
+        <input type="text" 
+        placeholder='first name'
+        onChange={changeHolder}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default App;
